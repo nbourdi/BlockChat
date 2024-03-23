7c13a98fd4988177aab995fac305333c5b63db61
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2024-03-23 20:40:10 +0200

    goofy ahh genesis

--- backend/genesis.py
+++ backend/genesis.py
@@ -3,6 +3,18 @@ port = 3000
 
 # stake = 
 # capacity = 
-
+from node import Node
 def main():
-    pass
+    # Create an instance of the Node class
+    node1 = Node(capacity=5, stake=10)
+
+    # Test some functionality of the Node class]
+    node1.id=1
+    print("Node ID:", node.id)
+    print("Node Wallet Public Key:", node.wallet.public_key)
+    print("Node Blockchain Length:", len(node.blockchain.blocks))
+    # You can continue testing other methods and attributes of the Node class here
+
+# Entry point of the script
+if __name__ == "__main__":
+    main()
