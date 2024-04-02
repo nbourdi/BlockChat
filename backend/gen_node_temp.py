221298f0004b61745867b15d50d120b99a6608c5
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2024-04-02 19:05:40 +0300

    Update gen_node_temp.py

--- backend/gen_node_temp.py
+++ backend/gen_node_temp.py
@@ -3,17 +3,16 @@ import requests
 import logging
 from node import Node
 from api import api, global_node
-import os
-import sys
+import threading
+import time
 
-bootstrap_ip = '0.0.0.0'
+
+bootstrap_ip = '127.0.0.1'
 bootstrap_port = 5000
-port = 5000
 node = global_node
-# is_bootstrap = os.environ.get('IS_BOOTSTRAP')
 is_bootstrap = "1"
 
-ip = '0.0.0.0'
+ip = '127.0.0.1'
 
 app = Flask(__name__)
 app.register_blueprint(api)
@@ -23,19 +22,14 @@ capacity = 10
 
 node = Node(capacity=capacity, stake=stake)
 
-# Configure logging
 logging.basicConfig(filename='record.log', level=logging.DEBUG)
 
-def main():
-    app.logger.debug("Node ID: %s", node.id)
-    app.logger.debug("Node Wallet Public Key: %s", node.wallet.public_key)
-    app.logger.debug("Node Blockchain Length: %s", len(node.blockchain.blocks))
+registration_completed = False  # Flag to track registration status
 
-if __name__ == "__main__":
 
-    
-    
-    app.logger.debug("im node1 test temp")
+def register_node1():
+    # Wait for a brief period to allow the Flask server to start running
+    time.sleep(1)  # Adjust the sleep duration as needed
 
     json_info = {
         'ip': "127.0.0.1",
@@ -43,11 +37,24 @@ if __name__ == "__main__":
         'pub_key': node.wallet.public_key
     }
 
-    url = f'http://127.0.0.1:5000/register_node' # NOT FOR DOCKER ONLY PROCESSES
+    url = f'http://{bootstrap_ip}:{bootstrap_port}/register_node'
     response = requests.post(url, json=json_info)
-    response_data = response.json()
+    
+    try:
+        response.raise_for_status()  # Raise an error for bad responses
+        response_data = response.json()
+        node.id = int(response_data.get('id'))
+        print("Registration completed.")
+    except Exception as e:
+        print(f"Failed to register node: {e}")
 
-    node.id = int(response_data['id'])
+
+if __name__ == "__main__":
+    app.logger.debug("im node1 test temp")
+
+    # Start a new thread for registering the node if registration is not completed
+    if not registration_completed:
+        registration_thread = threading.Thread(target=register_node1)
+        registration_thread.start()
+
     app.run(host="127.0.0.1", port=5001)
-
-    main()
