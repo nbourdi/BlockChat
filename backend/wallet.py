9f057069a34513c15cf1b5066596355c1807dca7
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2024-04-02 21:36:55 +0300

    Update wallet.py

--- backend/wallet.py
+++ backend/wallet.py
@@ -2,13 +2,10 @@ from Crypto.PublicKey import RSA
 
 class Wallet:
 
-    def generate_wallet(self):
+    def __init__(self):
         # Generate RSA key pair
         private_key = RSA.generate(2048)
         public_key = private_key.publickey()
         self.private_key = private_key.export_key(format='PEM').decode()
         self.public_key = public_key.export_key(format='PEM').decode()
-        
-    def init(self):
-        self.generate_wallet()
-        self.balance = 0 # i added that, balance = blockchat coins
+        self.balance = 0  # Added this, assuming it's related to the wallet
